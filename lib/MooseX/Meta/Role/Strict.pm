2751aa107e7eab8c4f8494b7ec1873cd63eef4a6
Curtis "Ovid" Poe <ovid@cpan.org>
2009-11-19 14:52:16 +0000

    Import of OVID/MooseX-Role-Strict-0.03 from CPAN.
    gitpan-cpan-distribution: MooseX-Role-Strict
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         OVID/MooseX-Role-Strict-0.03.tar.gz
    gitpan-cpan-author:       OVID
    gitpan-cpan-maturity:     released

--- lib/MooseX/Meta/Role/Strict.pm
+++ lib/MooseX/Meta/Role/Strict.pm
@@ -3,30 +3,19 @@ package MooseX::Meta::Role::Strict;
 use Moose;
 extends 'Moose::Meta::Role';
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
-sub apply {
+override apply => sub {
     my ( $self, $other, @args ) = @_;
 
-    ( blessed($other) )
-      || Moose->throw_error("You must pass in an blessed instance");
-
-    if ( $other->isa('Moose::Meta::Role') ) {
-        require Moose::Meta::Role::Application::ToRole;
-        return Moose::Meta::Role::Application::ToRole->new(@args)
-          ->apply( $self, $other );
-    }
-    elsif ( $other->isa('Moose::Meta::Class') ) {
+    if ( blessed($other) && $other->isa('Moose::Meta::Class') ) {
         # already loaded
         return MooseX::Meta::Role::Application::ToClass::Strict->new(@args)
           ->apply( $self, $other );
     }
-    else {
-        require Moose::Meta::Role::Application::ToInstance;
-        return Moose::Meta::Role::Application::ToInstance->new(@args)
-          ->apply( $self, $other );
-    }
-}
+
+    super;
+};
 
 1;
 
@@ -38,7 +27,7 @@ MooseX::Meta::Role::Strict - Ensure we use strict role application.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =head1 DESCRIPTION
 
